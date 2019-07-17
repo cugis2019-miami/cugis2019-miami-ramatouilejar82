572d6730ec23fc260ce497712285b597409709c7
ramatouilejar82 <53005825+ramatouilejar82@users.noreply.github.com>
2019-07-17 09:56:02 -0400

    Add files via upload

---
+++ Ramatoulie Jarjue.py
@@ -0,0 +1,49 @@
+
+print("I am learing to code in python today")
+dprint(53)
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print((8/9)
+print("the division of 5/2 is,"5/2)
+def plus(a,b):
+ plus= a+b
+ Pls(2,3)
+def plus(c,b,a):
+plus=c,b,a 
+print("plus")
+ plus(5,6,3)
+def triangle(b,h):
+triangle=(1/2)*b*h
+print(triangle)
+triangle(5,10)
+25 
+box=5
+print ("there are",box,"bars in the box")
+box=5+5
+print(box )
+D="dark"
+M="mike"
+w="white"
+print(d,m,)
+def greeting (name):
+    print(name)
+    
+greeting("kim")
+math,factorial(1)
+math.pow(2,4)
+def cube(y):
+    cube=math.pow(y,(1/3))
+    print("the cube roof of",y,"is",cube)
+    
+    cube(8)
+    
+import math
+print("please enter a value")
+y = 20
+def cube(y):
+    cube=math.pow(y,(1/3))
+    print("the cube roof of",y,"is",cube)
+
+cube(y)
