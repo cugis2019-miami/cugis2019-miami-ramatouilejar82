adcc044cd1488b44b19cca415e50cb34e67b0e74
ramatouilejar82 <53005825+ramatouilejar82@users.noreply.github.com>
2019-07-17 16:17:58 -0400

    Add files via upload

---
+++ Ramatoulie Jarjue Day 3.py
@@ -0,0 +1,77 @@
+
+print("I am learing to code in python today")
+dprint(53)
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print((8/9)
+print("the division of 5/2 is,"5/2)
+def plus(a,b):
+ plus= a+b
+ Pls(2,3)
+def plus(c,b,a):
+plus=c,b,a 
+print("plus")
+ plus(5,6,3)
+def triangle(b,h):
+triangle=(1/2)*b*h
+print(triangle)
+triangle(5,10)
+25 
+box=5
+print ("there are",box,"bars in the box")
+box=5+5
+print(box )
+D="dark"
+M="mike"
+w="white"
+print(d,m,)
+def greeting (name):
+    print(name)
+    
+greeting("kim")
+math,factorial(1)
+math.pow(2,4)
+def cube(y):
+    cube=math.pow(y,(1/3))
+    print("the cube roof of",y,"is",cube)
+
+    cube(8)
+    
+import math
+print("please enter a value")
+y = 20
+def cube(y):
+    cube=math.pow(y,(1/3))
+    print("the cube roof of",y,"is",cube)
+
+cube(y)
+print("the of milk chocolate in chocolate box is",chocolatebox["milk"])
+
+studentType1 = male
+studentType2= female
+studentType3= male
+studentType4= female
+  
+age={"steve":32,"lia":28,"vin":45,"katie":38}
+gender = {"steve":"male", "lia":"female","vin":"male","katie":"female"}
+print("the age of lia is",age["lia"], "and her gender is", gender["lia"])
+
+import pandas
+dir ("pandas")
+
+chipscans={"hotchips":10,"limechips":15,"chips":25}
+chipTyper=pandas.Series(chipscans)
+print(chipTyper)
+
+studentallinfo=[["Kim",40,"female"],["Laila",25,"female"],["Amita",32,"female"]]
+df1=pandas.DataFrame(studentallinfo)
+print(df)
+
+df2=pandas.DataFrame(studentallinfo,colums=["name","Age","Gender"])
+Print(df2)
+
+
+
+
